91383242a44fc36b1f3b91f2869b788c1c004f31
Kabilan1719 <rkabilan1719@gmail.com>
2026-03-24 22:36:20 +0530

    10

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter } from "react-router-dom"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const basename =
